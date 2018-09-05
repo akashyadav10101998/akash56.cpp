62f7bcd747a3d74509e64f3d7e9780e1e5445ad4
akashyadav10101998 <42208424+akashyadav10101998@users.noreply.github.com>
2018-09-05 16:21:15 +0530

    Add files via upload

---
+++ loop2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n;
+cout << "print natural numbers in reverse direction";
+cout << "enter the number";
+cin >> n;
+int i = n;
+while (i >= 1)
+{ 
+cout << "value of number" <<i<< endl;
+i =i-1;
+}
+return 0;
+}  
